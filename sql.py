358f407ad4e632070f9b370dfd187bd308005640
Stef Roes <info@roeswebdesign.nl>
2018-09-28 11:49:13 +0200

    MYSQL init

---
+++ sql.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+# --- MYSQL INSTALLEREN
+# GA NAAR FILE -> SETTINGS -> PROJECT -> PROJECT INTERPRETER -> KLIK RECHTS OP HET PLUS ICON
+# ZOEK NAAR 'mysql-connector', KIES DEZE EN KLIK OP 'Install Package'
+
+
+# --- INLOGGEN OP DATABASE
+# https://roeswebdesign.nl:8443/domains/databases/phpMyAdmin/
+# username: fietsen_user
+# password: QYm6Pt3Cv4cDNynT
+
+
+db = mysql.connector.connect(
+    host='37.97.240.38',
+    user='fietsen_user',
+    passwd='QYm6Pt3Cv4cDNynT',
+    database='fietsenstalling'
+)
+
+cursor = db.cursor()
+
+cursor.execute('SELECT name FROM user')
+
+result = cursor.fetchall()
+
+for i in result:
+    print(i)
