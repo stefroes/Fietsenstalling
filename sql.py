4fcc882bb3dcabfba2c8b0fdd3e1ba4443afa7d7
laurensvandehulst <laurensvandehulst@gmail.com>
2018-09-29 15:29:51 +0200

    Ben wezen zoeken naar iets om dit:
    FirstName = str(input("Vul hier uw voornaam in: "))
    LastName = str(input("Vul hier uw achternaam in: "))
    Adress = (input("Vul hier uw postcode in: "))
    Huisnummer = eval(input("Vul hier uw huisnummer in:"))
    Email = str(input("Vul hier uw email in: "))
    
    Te linken aan de waarden bij "cursor.execute()(zoals voornaam, achternaam etc.) maar krijg het niet voor elkaar. Misschien kan een van jullie het? hij

--- sql.py
+++ sql.py
@@ -21,10 +21,21 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-cursor.execute("INSERT INTO user(name) VALUES('laurens');")
-
+cursor.execute(
+    "INSERT INTO user(voornaam) VALUES('laurens')"
+    "INSERT INTO user(tussenvoegsels) Values('van de')"
+    "INSERT INTO user(achternaam) VALUES('Hulst')"
+    "INSERT INTO user(postcode) VALUES('7313BR')"
+    "INSERT INTO user(huisnummer) VALUES('15')"
+    "INSERT INTO user(email) VALUES('laurensvandehulst@gmail.com')"
+)
 db.commit()
 db.close()
 
-
-
+# ben wezen zoeken naar iets om dit:
+#FirstName = str(input("Vul hier uw voornaam in: "))
+#LastName = str(input("Vul hier uw achternaam in: "))
+#Adress = (input("Vul hier uw postcode in: "))
+#Huisnummer = eval(input("Vul hier uw huisnummer in:"))
+#Email = str(input("Vul hier uw email in: "))
+#in te linken aan de waarden bij "cursor.execute() maar krijg het niet voor elkaar. Misschien kan een van jullie het?
