7e31801d3fa0dd29097883bcd2ae32a020e83ef1
laurensvandehulst <laurensvandehulst@gmail.com>
2018-10-01 15:34:14 +0200

    keuzemenu gemaakt

--- sql.py
+++ sql.py
@@ -10,14 +10,20 @@ import mysql.connector
 # https://roeswebdesign.nl:8443/domains/databases/phpMyAdmin/
 # username: fietsen_user
 # password: QYm6Pt3Cv4cDNynT
+print("Welkom bij de NS - fietsenstalling.\n")
+
+new_customer = input('Heeft u al een account bij de NS-fietsenstalling? Type ja of nee: ')
+
+if new_customer == 'nee' or 'Nee':
+    print('\nOm uw fiets te kunnen stallen in de NS-fietsenstalling dient u eerst een account aan te maken.\n')
+    first_name = input('Vul hier uw voornaam in: ').capitalize()
+    insertion = input('Vul hier uw tussenvoegsels in: ')
+    last_name = input('Vul hier uw achternaam in: ').capitalize()
+    zip = input('Vul hier uw postcode in: ').replace(' ', '').upper()
+    number = input('Vul hier uw huisnummer in: ')
+    email = input('Vul hier uw e-mail in: ').lower()
 
 
-first_name = input('Vul hier uw voornaam in: ').capitalize()
-insertion = input('Vul hier uw tussenvoegsels in: ')
-last_name = input('Vul hier uw achternaam in: ').capitalize()
-zip = input('Vul hier uw postcode in: ').replace(' ', '').upper()
-number = input('Vul hier uw huisnummer in: ')
-email = input('Vul hier uw e-mail in: ').lower()
 
 db = mysql.connector.connect(
     host='37.97.240.38',
@@ -30,6 +36,10 @@ cursor = db.cursor()
 cursor.execute('INSERT INTO user(first_name, insertion, last_name, zip, number, email) VALUES(%s, %s, %s, %s, %s, %s)', (first_name, insertion, last_name, zip, number, email))
 db.commit()
 
-print(cursor.rowcount, 'record inserted.')
+print(cursor.rowcount, 'record inserted.\n')
+
+if cursor.rowcount == 1:
+    print("Om uw fiets te kunnen stallen, dient u eerst in te loggen. Volg alstublieft de volgende stappen:\n")
 
 db.close()
+
