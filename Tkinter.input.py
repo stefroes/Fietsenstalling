94d65086fd110482e5f0f1b0e05b8add8930a444
Stef Roes <info@roeswebdesign.nl>
2018-10-18 14:32:19 +0200

    Cleanup

--- Tkinter.input.py
+++ Tkinter.input.py
@@ -1,17 +1,13 @@
 from tkinter import *
 import db_connect
 
-def login():
-    login.grid()
-
 blauw = '#003373'
 geel = '#ffac00'
 
 gui = Tk()
-gui.geometry("700x650")
+gui.attributes('-fullscreen', True)
+gui.geometry('700x650')
 gui.configure(bg=geel)
-
-
 
 db = db_connect.db
 
@@ -23,9 +19,6 @@ var3 = StringVar()
 var4 = StringVar()
 var5 = StringVar()
 var6 = StringVar()
-# var2 = StringVar()
-username = None               # Global variable declaration
-# password = None
 
 login = Frame(master=gui)
 Label(gui, width=15, text="Voornaam: ", bg=geel).grid(row=0, column=0)
@@ -38,7 +31,7 @@ Label(gui, width=15, text="Achternaam: ", bg=geel).grid(row=2, column=0)
 last_name_entry = Entry(gui, width=29, textvariable=var3).grid(row=2, column=1)
 
 Label(gui, width=15, text="Postcode: ", bg=geel).grid(row=3, column=0)
-zip_entry= Entry(gui, width=29, textvariable=var4).grid(row=3, column=1)
+zip_entry = Entry(gui, width=29, textvariable=var4).grid(row=3, column=1)
 
 Label(gui, width=15, text="Huisnummer: ", bg=geel).grid(row=4, column=0)
 housenumber_entry = Entry(gui, width=29, textvariable=var5).grid(row=4, column=1)
@@ -47,12 +40,7 @@ Label(gui, width=15, text="Email: ", bg=geel).grid(row=5, column=0)
 email_entry = Entry(gui, width=29, textvariable=var6).grid(row=5, column=1)
 
 
-
-# Label(gui, text="Password: ").grid(row=1, sticky=W)
-# e2 = Entry(gui, textvariable=var2).grid(row=1, column=1)
-
 def enter():
-    #global first_name_entry, insertion_entry, last_name_entry, zip_entry, housenumber_entry, email_entry
     print(first_name_entry)
 
     first_name = var1.get()
@@ -62,20 +50,19 @@ def enter():
     housenumber = var5.get()
     email = var6.get()
 
-    print(username)
-    # print(password)
-
     cursor = db.cursor()
     cursor.execute("INSERT INTO user(unique_code, first_name, insertion, last_name, zip, streetnumber, email, date_time) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)", ('ABC', first_name, insertion, last_name, zip, housenumber, email, '2018-10-16 12:00:00'))
     db.commit()
 
     print(cursor.rowcount)
 
-def printIt():
-    print('Kan ff niet nu')
 
-Button(gui, width=15, text="Inloggen" , highlightbackground=blauw, bg=blauw, command=printIt).grid(row=6, column=0)
-Button(gui, width=15, text="Registreren" ,highlightbackground=blauw, bg=blauw, command=enter).grid(row=6, column=1)
-
+Button(gui, width=15, text="Inloggen", highlightbackground=blauw, bg=blauw).grid(row=6, column=0)
+Button(gui, width=15, text="Registreren", highlightbackground=blauw, bg=blauw, command=enter).grid(row=6, column=1)
 
 gui.mainloop()
+
+
+class main_screen:
+    def __init__(self, master):
+        self.master = master
